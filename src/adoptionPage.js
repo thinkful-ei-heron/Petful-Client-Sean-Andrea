07191f6ef4ba0514c5cfd6db63aabfca10fb5196
seank329 <seankamran@gmail.com>
2019-12-20 19:20:42 -0500

    working on displaying animals

--- src/adoptionPage.js
+++ src/adoptionPage.js
@@ -1,26 +1,8 @@
-<<<<<<< HEAD
 import React, {Component} from 'react'
 import { Queue, adoptionQueue, namesArray } from './Queue'
 import PetfulApi from './Services/petful-api-service'
-=======
-import React, { Component } from 'react';
-import { Queue, adoptionQueue } from './Queue';
-import './adoptionPage.css';
->>>>>>> d8e6f21c11a744c2397a1749fc3a1f3b08a7490c
 
 class AdoptionPage extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			wantsToRegister: false,
-			firstName: '',
-			lastName: '',
-			registered: false,
-			namesDatabase: [],
-			numberInLine: '',
-			Q: ''
-		};
-	}
 
     constructor(props){
         super(props);
@@ -35,13 +17,12 @@ class AdoptionPage extends React.Component {
         }
     }
 
-		setInterval(changeList);
-	}
 
 	addToQ = (name) => {
 		adoptionQueue.enqueue(name);
-	};
-
+    };
+    
+    componentDidMount(){
         PetfulApi.getAllCats()
             .then(cat => this.setState({catQ:cat}))
             .catch({error:'An Error has Occurred'})
@@ -92,40 +73,15 @@ class AdoptionPage extends React.Component {
         e.preventDefault()
         this.setState({lastName:e.target.value})
     }
-    
-    render(){
-        let cat = this.state.catQ[0]
-        return(
-            
-        <div className = 'adoptionPage'>
-            <div className = 'adoption'>
-                <h1> Welcome to Petful Adoption Center! </h1>
-                <h2> Adoption Page </h2>
-                <div className = 'dog'>
-                    <h3>Dogs</h3>
-                </div>
-                <div className = 'cat'>
-                    <h3>Cats</h3>
-                    <h4></h4>
-                </div>
-                <button type='button' onClick={this.clickHandler}>Register</button>
-            </div>
-            {this.state.wantsToRegister === true ? 
-            <div className = 'registration'>
-                <form>
-                    <p>Please enter your first name: <input type='text' placeholder='First Name' name='firstName' onChange={this.handleFirstName}></input></p>
-                    <p>Please enter your last name:  <input type='text' placeholder='Last Name'name='lastName' onChange={this.handleLastName}></input></p>
-                    <button type='button' onClick={this.submitHandler}>Register For Adoption!</button>   
-                </form> 
-            </div> : null }
-            {this.state.registered ? 
-             <div className = 'registeredUser'>
-                <h4>Thank you for your interest, {this.state.firstName} {this.state.lastName}! You are currently number {adoptionQueue.size()} in line</h4>
-         </div> : null }
-    
-        </div>
-    )
-    }
+
+    // showDogs=()=>{
+    //     let temp = this.state.dogQ.map((dog, index) => {
+    //         return(
+    //             <li key={index}>{dog.name}</li>
+    //            )
+    //          })
+    //     //return temp
+    // }
 
 	render() {
 		return (
@@ -135,6 +91,7 @@ class AdoptionPage extends React.Component {
 					<h2> Adoption Page </h2>
 					<div className="dog">
 						<h3>Dogs</h3>
+                         {}
 					</div>
 					<div className="cat">
 						<h3>Cats</h3>
