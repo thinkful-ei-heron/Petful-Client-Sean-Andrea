4f07c619e2c346986185ddb7261d98581ca6e98f
Andrea Bender <andreab.1726@gmail.com>
2019-12-23 08:49:54 -0700

    css fixed

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-	API_PRODUCTION_ENDPOINT: 'https://petful-server-sean-andrea.herokuapp.com/api',
-	API_ENDPOINT: 'http://localhost:8080/api'
+	// API_PRODUCTION_ENDPOINT: 'https://petful-server-sean-andrea.herokuapp.com/api',
+	API_ENDPOINT: process.env.REACT_APP_API_BASE || 'http://localhost:8080/api'
 };
